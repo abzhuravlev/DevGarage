6fb4e4cb1ac8df3312554bcca0ce51d5fcc441b9
abzhuravlev <127055210+abzhuravlev@users.noreply.github.com>
2023-03-06 22:27:03 +0300

    Update ConsumerServiceImpl.java

--- node/src/main/java/ru/zhuravlev/service/impl/ConsumerServiceImpl.java
+++ node/src/main/java/ru/zhuravlev/service/impl/ConsumerServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.zhuravlev.service.impl;
 
-import lombok.extern.log4j.Log4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -10,7 +9,6 @@ import ru.zhuravlev.service.MainService;
 import static ru.zhuravlev.model.RabbitQueue.*;
 
 @Service
-@Log4j
 public class ConsumerServiceImpl implements ConsumerService {
     private final MainService mainService;
 
