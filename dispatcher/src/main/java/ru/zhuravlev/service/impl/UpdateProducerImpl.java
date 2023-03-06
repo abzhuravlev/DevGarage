a24b9c53fbd19e46f0f226601c1762161fff44d7
abzhuravlev <127055210+abzhuravlev@users.noreply.github.com>
2023-03-06 22:00:43 +0300

    Update UpdateProducerImpl.java

--- dispatcher/src/main/java/ru/zhuravlev/service/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/ru/zhuravlev/service/impl/UpdateProducerImpl.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.zhuravlev.service.UpdateProducer;
 @Service
-@Log4j
 public class UpdateProducerImpl implements UpdateProducer {
     private final RabbitTemplate rabbitTemplate;
 
